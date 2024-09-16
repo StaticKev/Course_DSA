37ae7f1cf6c580a7a14abbe1d3190c7012a187af
StaticKev <statickev321@gmail.com>
2024-09-16 07:49:24 +0700

    Another additions!

--- src/Tree/Impl_BTree.java
+++ src/Tree/Impl_BTree.java
@@ -4,60 +4,59 @@ import java.lang.reflect.Array;
 
 class MyBTree<T extends Comparable<T>> {
     class BTreeNode {
-        T[] keys;
-        BTreeNode[] childs;
-        int keyCount;
+        int t;
+        final T[] keys;
+        final BTreeNode[] children;
+        int keysCount;
         boolean isLeaf;
 
         @SuppressWarnings("unchecked")
         public BTreeNode(int t, boolean isLeaf) {
-            keys = (T[]) new Object[2 * t - 1];
-            childs = (BTreeNode[]) Array.newInstance(BTreeNode.class, 2 * t);
-            keyCount = 0;
+            this.t = t;
+            this.keys = (T[]) new Object[2 * t - 1];
+            this.children = (BTreeNode[]) Array.newInstance(BTreeNode.class, 2 * t);
+            this.keysCount = 0;
             this.isLeaf = isLeaf;
         }
 
+        public void insertNonFull(T value) {
+            int i = keysCount - 1;
+
+        }
+
+        // Method untuk memasukkan value dalam sebuah node.
         public void insertKey(T value) {
-            if (value == null) throw new IllegalArgumentException("Null value not allowed here!");
+            // Cek seluruh key pada node ini, menghasilkan 4 skenario:
+            // 1) Jika nilai pada indeks ke-i adalah null maka tugaskan nilai yang ingin
+            //    dimasukkan pada indeks tersebut. Karena pengisian key dimulai dari indeks awal,
+            //    dan insertion dan deletion hanya perlu menggeser key ke kanan atau ke kiri maka,
+            //    tidak mungkin ada elemen dengan nilai non-null di sebelah kanan elemen dengan
+            //    nilai null.
+            // 2) Jika nilai pada indeks ke-1 sama dengan nilai yang ingin dimasukkan, return.
+            //    Karena tidak boleh ada nilai duplikat dalam tree.
+            // 3) Jika key yang dibandingkan lebih kecil, lanjutkan pencarian.
+            // 4) Key yang dibandingkan lebih besar, geser elemen dengan indeks 'i' dan seluruh
+            //    elemen di sebelah kanannya sebanyak 1 indeks ke kanan. (NOTE: Kecualikan indeks
+            //    terakhir saat menggeser!)
+            for (int i = 0; i < keysCount; i++) {
+                if (keys[i] != null) {
+                    // Nilai duplikat
+                    if (value.compareTo(keys[i]) == 0) return;
+                    // Nilai lebih kecil dari key
+                    else if (value.compareTo(keys[i]) < 0) continue;
 
-            if (!(keyCount == keys.length)) {
-
-                for (int i = 0; i < keyCount; i++) {
-                    if (keys[i] != null) {
-                        // Base case
-                        if (value.compareTo(keys[i]) == 0) return;
-                        else if (value.compareTo(keys[i]) <= 0) continue;
-
-                        // Shift the elements
-                        for (int j = keyCount - 1; j >= i; j--) {
-                            keys[j + 1] = keys[j];
-                        }
-
+                    // Menggeser elemen
+                    for (int j = keysCount - 2; j >= i; j--) {
+                        keys[j + 1] = keys[j];
                     }
-
-                    // Assign the value into a specific index
-                    keys[i] = value;
-                    keyCount++;
-                    break;
                 }
 
-            } else {
-                for (int i = 0; i < keyCount; i++) {
-//                    if ()
-                }
-                // root harus diganti
-                insertKey(root, value);
+                // Menugaskan nilai pada indeks tertentu pada 'keys'.
+                keys[i] = value;
+                keysCount++;
+                break;
             }
         }
-
-        private void insertKey(BTreeNode node, T value) {
-
-        }
-
-        public void traverse() {}
-
-        public BTreeNode search() { return null; }
-
     }
 
     int t; // Minimum degree
@@ -69,16 +68,13 @@ class MyBTree<T extends Comparable<T>> {
     }
 
     void insert(T value) {
-        root = insert(root, value);
+        if (value == null) throw new IllegalArgumentException("Null value not allowed here!");
+
+        insert(root, value);
     }
 
-    private BTreeNode insert(BTreeNode node, T value) {
-//        if (node == null) return new BTreeNode(t, value);
-//        else {
-//
-//
-//            return node;
-//        }
+    private void insert(BTreeNode node, T value) {
+        
     }
 }
 
